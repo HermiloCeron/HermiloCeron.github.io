23b6492506090ca8a717224355a7afd302e7ee32
ceronhermilo <ceronhermilo@yahoo.com>
2021-01-09 19:39:25 -0600

    Working on the game logic

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ iniArray();
 // Draw the initial board
 drawBoard();
 
+// Main function to play
+playHanoi();
+
 // Initializate the array in the traditional way
 function iniArray(){
     for(let i=0;i<elements;i++){
@@ -25,9 +28,48 @@ function drawBoard(){
         for(let j=0;j<boardArray[i].length;j++){
             let btn=document.createElement('button');
             btn.className='element';
-            btn.style.width=(10+((90-10)/elements)*boardArray[i][j])+"%";
+            btn.id='element'+boardArray[i][j];
+            btn.style.width=(10+((90-10)/(elements-1))*boardArray[i][j])+"%";
             console.log('value'+btn.style.width)
             document.querySelector('#ctn'+i).appendChild(btn);
         }
     }
+}
+
+//Main function to play
+function playHanoi(){
+    for(let i=0;i<3;i++){
+        // Check if there is an element inside the container
+        if(boardArray[i].length>0){
+            // Select the top item at the container
+            let top=document.querySelector('#element'+boardArray[i][0]);
+            // Assign an action when you click in such element
+            top.addEventListener('click',topFunction,false)
+        }
+    }
+}
+
+function topFunction(){
+    //console.log(e.target);
+    //Remove event listener of the tops
+    for(let j=0;j<3;j++){
+        // Check if there is an element inside the container
+        if(boardArray[j].length>0){
+            let top2=document.querySelector('#element'+boardArray[j][0]);
+            console.log(top2);
+            top2.removeEventListener('click',topFunction,false);
+        }
+
+    }
+    
+    for(let j=0;j<3;j++){
+        // Select a container
+        let container=document.querySelector('#ctn'+j);
+        // Add an event listeenr to teh container
+        container.addEventListener('click',selectContainer,false);
+    }
+}
+
+function selectContainer(evt){
+    console.log('hola');
 }
